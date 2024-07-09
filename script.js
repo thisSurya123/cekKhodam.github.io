00f62b0ee8ffdc0be7f819bdf74b5744f3884699
Surya ahmad afandi <thissurya31@gmail.com>
2024-07-09 22:06:12 +0700

    update

--- script.js
+++ script.js
@@ -2,42 +2,162 @@ const khodam = [
     {
         'khodam' : 'Roger Sumatra',
         'img' : 'https://media1.tenor.com/m/r_mWvlKWHPAAAAAC/wolf-big-dog.gif',
-        'desc' : 'didalam diri kamu ada sosok serigala kesepian yang mendampingimu'
-    },
-    {
-        'khodam' : 'Babi Icikiwir',
-        'img' : 'https://i.pinimg.com/564x/0f/86/6c/0f866ce4373f288aca970cae971891fc.jpg',
-        'desc' : 'Icikiwir merupakan jelmaan siluman motor'
-    },
-    {
-        'khodam' : 'Burung Papilo',
-        'img' : 'https://i.pinimg.com/564x/51/fe/07/51fe078c2cb0f0689cfda55cb13b1b3d.jpg',
-        'desc' : 'loremLorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur magnam dolor rem hic in tenetur ex ut voluptas alias, ea voluptates iure autem quidem nesciunt nulla at deserunt excepturi consequatur.'
-    },
-    {
-        'khodam' : 'scp 004',
-        'img' : 'https://i.pinimg.com/564x/67/ab/77/67ab77168e5b4d267fa7065771a07cb3.jpg',
-        'desc' : 'loremLorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur magnam dolor rem hic in tenetur ex ut voluptas alias, ea voluptates iure autem quidem nesciunt nulla at deserunt excepturi consequatur.'
-    },
-    {
-        'khodam' : 'Skeleton Mewing',
-        'img' : 'https://i.pinimg.com/originals/e8/a0/1b/e8a01bb33f2b4d505dc8dbb0f5022efa.gif',
-        'desc' : 'loremLorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur magnam dolor rem hic in tenetur ex ut voluptas alias, ea voluptates iure autem quidem nesciunt nulla at deserunt excepturi consequatur.'
-    },
-    {
-        'khodam' : 'Pinguin bandar',
-        'img' : 'https://i.pinimg.com/564x/77/41/a0/7741a0ea948c9bf5c7ae2ead23b754f3.jpg',
-        'desc' : 'loremLorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur magnam dolor rem hic in tenetur ex ut voluptas alias, ea voluptates iure autem quidem nesciunt nulla at deserunt excepturi consequatur.'
-    },
-    {
-        'khodam' : 'The Handsome Squidward',
-        'img' : 'https://i.pinimg.com/564x/80/a1/b4/80a1b4ea8681264dfc843b51c94e79db.jpg',
-        'desc' : 'loremLorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur magnam dolor rem hic in tenetur ex ut voluptas alias, ea voluptates iure autem quidem nesciunt nulla at deserunt excepturi consequatur.'
+        'desc' : 'Kesepian, jomblo dan gapunya kawan',
+        'status' : {
+            'rarity' : 'C'
+        }
     },
     {
         'khodam' : 'Kosong',
         'img' : 'https://i.pinimg.com/564x/6f/0c/7d/6f0c7dd236a49fef3d2c7ad9def7f87c.jpg',
-        'desc' : 'loremLorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur magnam dolor rem hic in tenetur ex ut voluptas alias, ea voluptates iure autem quidem nesciunt nulla at deserunt excepturi consequatur.'
+        'desc' : 'Saat ini anda tidak memiliki khodam yang berada disisi anda saat ini coba lagi kapan kapan',
+        'status' : {
+            'rarity' : 'E'
+        }
+    },
+    {
+        'khodam' : 'Kosong',
+        'img' : 'https://i.pinimg.com/564x/6f/0c/7d/6f0c7dd236a49fef3d2c7ad9def7f87c.jpg',
+        'desc' : 'Saat ini anda tidak memiliki khodam yang berada disisi anda saat ini coba lagi kapan kapan',
+        'status' : {
+            'rarity' : 'E'
+        }
+    },
+    {
+        'khodam' : 'Babi Icikiwir',
+        'img' : 'https://i.pinimg.com/564x/0f/86/6c/0f866ce4373f288aca970cae971891fc.jpg',
+        'desc' : 'strees, hobi ngutang dan biasanya memiliki gangguan kejiwaan',
+        'status' : {
+            'rarity' : 'C'
+        }
+    },
+    {
+        'khodam' : 'Babi Icikiwir',
+        'img' : 'https://i.pinimg.com/564x/0f/86/6c/0f866ce4373f288aca970cae971891fc.jpg',
+        'desc' : 'strees, hobi ngutang dan biasanya memiliki gangguan kejiwaan',
+        'status' : {
+            'rarity' : 'C'
+        }
+    },
+    {
+        'khodam' : 'Babi Icikiwir',
+        'img' : 'https://i.pinimg.com/564x/0f/86/6c/0f866ce4373f288aca970cae971891fc.jpg',
+        'desc' : 'strees, hobi ngutang dan biasanya memiliki gangguan kejiwaan',
+        'status' : {
+            'rarity' : 'C'
+        }
+    },
+    {
+        'khodam' : 'Burung Sodikin',
+        'img' : 'https://i.pinimg.com/564x/51/fe/07/51fe078c2cb0f0689cfda55cb13b1b3d.jpg',
+        'desc' : 'pembawa sial, tidak disukai admin, jarang mandi dan jomblo',
+        'status' : {
+            'rarity' : 'E'
+        }
+    },
+    {
+        'khodam' : 'Burung Sodikin',
+        'img' : 'https://i.pinimg.com/564x/51/fe/07/51fe078c2cb0f0689cfda55cb13b1b3d.jpg',
+        'desc' : 'pembawa sial, tidak disukai admin, jarang mandi dan jomblo',
+        'status' : {
+            'rarity' : 'E'
+        }
+    },
+    {
+        'khodam' : 'Burung Sodikin',
+        'img' : 'https://i.pinimg.com/564x/51/fe/07/51fe078c2cb0f0689cfda55cb13b1b3d.jpg',
+        'desc' : 'pembawa sial, tidak disukai admin, jarang mandi dan jomblo',
+        'status' : {
+            'rarity' : 'E'
+        }
+    },
+    {
+        'khodam' : 'Burung Sodikin',
+        'img' : 'https://i.pinimg.com/564x/51/fe/07/51fe078c2cb0f0689cfda55cb13b1b3d.jpg',
+        'desc' : 'pembawa sial, tidak disukai admin, jarang mandi dan jomblo',
+        'status' : {
+            'rarity' : 'E'
+        }
+    },
+    {
+        'khodam' : 'Burung Sodikin',
+        'img' : 'https://i.pinimg.com/564x/51/fe/07/51fe078c2cb0f0689cfda55cb13b1b3d.jpg',
+        'desc' : 'pembawa sial, tidak disukai admin, jarang mandi dan jomblo',
+        'status' : {
+            'rarity' : 'E'
+        }
+    },
+    {
+        'khodam' : 'Jawir Ireng',
+        'img' : 'https://i.pinimg.com/564x/67/ab/77/67ab77168e5b4d267fa7065771a07cb3.jpg',
+        'desc' : 'rezekinya lancar, sering makan nasi kuning, kurus, dan berpendidikan tinggi',
+        'status' : {
+            'rarity' : 'A'
+        }
+    },
+    {
+        'khodam' : 'Jawir Ireng',
+        'img' : 'https://i.pinimg.com/564x/67/ab/77/67ab77168e5b4d267fa7065771a07cb3.jpg',
+        'desc' : 'rezekinya lancar, sering makan nasi kuning, kurus, dan berpendidikan tinggi',
+        'status' : {
+            'rarity' : 'A'
+        }
+    },
+    {
+        'khodam' : 'Jawir Ireng',
+        'img' : 'https://i.pinimg.com/564x/67/ab/77/67ab77168e5b4d267fa7065771a07cb3.jpg',
+        'desc' : 'rezekinya lancar, sering makan nasi kuning, kurus, dan berpendidikan tinggi',
+        'status' : {
+            'rarity' : 'A'
+        }
+    },
+    {
+        'khodam' : 'Skeleton Mewing',
+        'img' : 'https://i.pinimg.com/originals/e8/a0/1b/e8a01bb33f2b4d505dc8dbb0f5022efa.gif',
+        'desc' : 'sigma, dingin, dan memiliki aura +100.0000.000 menakutkan',
+        'status' : {
+            'rarity' : 'A'
+        }
+    },
+    {
+        'khodam' : 'Skeleton Mewing',
+        'img' : 'https://i.pinimg.com/originals/e8/a0/1b/e8a01bb33f2b4d505dc8dbb0f5022efa.gif',
+        'desc' : 'sigma, dingin, dan memiliki aura +100.0000.000 menakutkan',
+        'status' : {
+            'rarity' : 'A'
+        }
+    },
+    {
+        'khodam' : 'Skeleton Mewing',
+        'img' : 'https://i.pinimg.com/originals/e8/a0/1b/e8a01bb33f2b4d505dc8dbb0f5022efa.gif',
+        'desc' : 'sigma, dingin, dan memiliki aura +100.0000.000 menakutkan',
+        'status' : {
+            'rarity' : 'A'
+        }
+    },
+    {
+        'khodam' : 'Gundul hitam',
+        'img' : 'https://i.pinimg.com/564x/7f/59/c6/7f59c605ff051583424d71b0a07e6105.jpg',
+        'desc' : 'tidak goodlooking, berwibawa, terkenal, memiliki 50 mclaren, 10 hectare kebun sawit dan rajin bersedekah',
+        'status' : {
+            'rarity' : 'B'
+        }
+    },
+    {
+        'khodam' : 'Squidward Simatupang',
+        'img' : 'https://i.pinimg.com/564x/80/a1/b4/80a1b4ea8681264dfc843b51c94e79db.jpg',
+        'desc' : 'tampan dan berani serta rajin menabung dan rezekinya lancar',
+        'status' : {
+            'rarity' : 'S'
+        }
+    },
+    {
+        'khodam' : 'Kosong',
+        'img' : 'https://i.pinimg.com/564x/6f/0c/7d/6f0c7dd236a49fef3d2c7ad9def7f87c.jpg',
+        'desc' : 'Saat ini anda tidak memiliki khodam yang berada disisi anda saat ini coba lagi kapan kapan',
+        'status' : {
+            'rarity' : 'E'
+        }
     }
 ]
 
@@ -46,6 +166,8 @@ const loading = document.getElementById('loading');
 const reaction = document.getElementById('reaction');
 const btn_khodam = document.getElementById('btn-khodam');
 const btn_cekLagi = document.getElementById('cek-lagi');
+const desc = document.getElementById('desc');
+const rare = document.getElementById('rare');
 
 
 async function handleClick(){
@@ -56,8 +178,10 @@ async function handleClick(){
     
     setTimeout(() => {
         const random = Math.floor(Math.random() * khodam.length);
-        showKhodam.innerHTML = khodam[random].khodam;
+        showKhodam.innerHTML = khodam[random].khodam    ;
         reaction.setAttribute('src', khodam[random].img);
+        desc.innerHTML = khodam[random].desc;
+        rare.innerHTML = 'Rarity : '+ khodam[random].status.rarity;
         
         btn_cekLagi.style.display = 'block';
         loading.style.display = 'none';
@@ -68,4 +192,11 @@ async function handleClick(){
     }, 1500);
 }
 
-console.log(khodam[3]);
+function reset(){
+    reaction.setAttribute('src', 'https://media.tenor.com/hqHvXd2lmT4AAAAi/thinking.gif');
+    btn_cekLagi.style.display = 'none';
+    btn_khodam.style.display = 'block';
+    desc.innerHTML = '';
+    rare.innerHTML = '';
+    showKhodam.innerHTML = '';
+}
